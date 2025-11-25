d91e89286067603925689a7631c1d21f9643cba0
waternksy <playgamesroyale2@gmail.com>
2025-11-25 14:39:53 +0530

    feat: displaying mock contacts using jinja

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 
 This project was created to understand Hypermedia Driven Application (HDA)
 
+## Notes
+
+1. The first roadblock that i encounter is to use templating language called jinja,
+   i was familar with flask.
+
+### Design choice
+
+1. Started with Jinja templating to render the contacts.html template which uses
+row.html to show contacts on server.
+
 ## Reference
 
 - [The Book](https://hypermedia.systems/)
--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello, World!</h1>"
+    return redirect("/contacts")
+
+
+@app.route("/contacts")
+def contacts():
+    """
+    Can have search query in /contacts route
+    Ex: https://example.com/contacts?q=12
+
+    If there is query parameter (q above), should filter
+    contacts to match the query. otherwise, should show
+    all contacts.
+    """
+    contacts = [
+        {
+            "first": "Borrel",
+            "last": "Load",
+            "phone": "8798299467",
+            "email": "borrel.load@gmail.com",
+        },
+        {
+            "first": "Borrel",
+            "last": "Sword",
+            "phone": "9832213683",
+            "email": "borrel.sword@gmail.com",
+        },
+    ]
+    return render_template("contacts.html", contacts=contacts)
 
 
 def main():
